8e7a06027e4357d50d47d7335d41ee3c731c1d23
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-09-09 11:48:12 +0900

    feat: new solution
    just count last update

--- bin/substring_without_repeating.rs
+++ bin/substring_without_repeating.rs
@@ -1,54 +1,21 @@
-use std::{char, collections::HashMap};
+use std::collections::HashMap;
 
 struct Solution;
 
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        if s.is_empty() {
-            return 0;
-        }
-        let mut position_mark: HashMap<char, usize> = HashMap::new();
-        let array: Vec<char> = s.chars().collect();
-
-        let mut max_count = 0;
-        let mut current_count = 0;
-
-        let mut start_index = 0;
-        let end_index = array.len();
-
-        'out: loop {
-            for index in start_index..end_index {
-                let current_key = array[index];
-                if position_mark
-                    .get(&current_key)
-                    .is_some_and(|oindex| *oindex < index)
-                {
-                    let renew_pos = position_mark[&current_key];
-                    if renew_pos < start_index {
-                        current_count += 1;
-                    }
-                    start_index = renew_pos + 1;
-                    position_mark.remove(&current_key);
-                    if current_count > max_count {
-                        max_count = current_count;
-                    }
-                    current_count = 0;
-                    continue 'out;
-                }
-                position_mark
-                    .entry(current_key)
-                    .and_modify(|cindex| *cindex = index)
-                    .or_insert(index);
-                current_count += 1;
+        let mut hash: HashMap<char, i32> = HashMap::new();
+        let mut ans = 0;
+        let mut lo = -1;
+        for (hi, ch) in s.chars().enumerate() {
+            // loop through chars with loop index being hi.
+            if let Some(i) = hash.insert(ch, hi as i32) {
+                // inserts ch and hi into map and it returns old index.
+                lo = lo.max(i); // Move the lo pointer to that old index.
             }
-            break;
+            ans = ans.max(hi as i32 - lo); // updates ans if hi - lo is greater than previous answer.
         }
-
-        if current_count > max_count {
-            max_count = current_count;
-        }
-
-        max_count
+        ans
     }
 }
 
