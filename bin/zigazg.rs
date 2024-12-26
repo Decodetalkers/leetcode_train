a308046418850a3334c1d64ccaa007e036ae6615
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-12-26 12:10:57 +0900

    feat: better solution

--- bin/zigazg.rs
+++ bin/zigazg.rs
@@ -6,36 +6,26 @@ impl Solution {
         if num_rows == 1 {
             return s;
         }
-        let mut dictronary: Vec<[char; 1000]> = vec![];
-        for _ in 0..num_rows {
-            dictronary.push(['0'; 1000]);
-        }
+        let mut dictronary: Vec<String> = vec![String::new(); num_rows as usize];
         let mut backing = false;
-        let mut current_list = 0;
         let mut current_row = 0;
         for c in s.chars() {
             if current_row == 0 {
                 backing = false;
             }
 
-            dictronary[current_row][current_list] = c;
+            dictronary[current_row].push(c);
             if (current_row + 1) % num_rows as usize == 0 {
                 backing = true;
             }
             // NOTE: need backing
             if backing {
-                current_list += 1;
                 current_row -= 1;
                 continue;
             }
             current_row += 1;
         }
-        let mut output = String::new();
-        for str in dictronary {
-            let output_tmp: String = str.iter().filter(|c| **c != '0').collect();
-            output.push_str(&output_tmp);
-        }
-        output
+        dictronary.concat()
     }
 }
 
